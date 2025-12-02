8a5c302717888216c410112d727bf3f1dccdf964
Tajim Islam <tjm4181@gmail.com>
2025-12-03 01:53:18 +0600

    menusection indexedit

--- app/Services/MenuSectionService.php
+++ app/Services/MenuSectionService.php
@@ -6,11 +6,26 @@ use Symfony\Component\HttpFoundation\Request;
 
 class MenuSectionService
 {
+    public function MenuSectionList($search = [], $is_paginate = true) : array
+    {
+        $status_code = $status_message = $response = '';
+        try {
+            $response = (new MenuSection())->MenuSectionList($search, $is_paginate);
+            $status_code = ApiService::API_SUCCESS;
+            $status_message = 'Data Found';
+        } catch(\Throwable $th){
+            $status_code = ApiService::API_SERVER_ERROR;
+            $status_message = $th->getMessage();
+        }
+
+        return [$status_code, $status_message, $response];
+    }
+
     public function storeMenuSection($request)
     {
         $status_code = $status_message = $error_message = null;
         [$rules, $messages] = RequestRules::menuSectionRules($request->all());
-        
+
         $validation = RequestRules::validate($request->all(), $rules, $messages);
         if ($validation[0] !== ApiService::API_SUCCESS) {
             $status_code = $validation[0];
@@ -33,4 +48,71 @@ class MenuSectionService
 
         return [$status_code, $status_message, $error_message];
     }
+
+    public function getMenuSectionById($id)
+    {
+        $status_code = $status_message = $menu_section = '';
+        try {
+            $menu_section = (new MenuSection())->getMenuSectionById($id);
+            $status_code = ApiService::API_SUCCESS;
+            $status_message = 'Data Found';
+        } catch (\Throwable $th) {
+            $status_code = ApiService::API_SERVER_ERROR;
+            $status_message = $th->getMessage();
+        }
+        return [$status_code, $status_message, $menu_section];
+    }
+
+    public function updateMenuSection($request, $id)
+    {
+        $status_code = $status_message = $error_message = null;
+
+        [$rules, $messages] = RequestRules::menuSectionRules($request->all(), $id);
+        $validation = RequestRules::validate($request->all(), $rules, $messages);
+
+        if ($validation[0] !== ApiService::API_SUCCESS) {
+            return [$validation[0], $validation[1], $validation[2]];
+        }
+
+        try {
+            $updated = (new MenuSection())->updateMenuSection($request, $id);
+
+            if ($updated) {
+                $status_code = ApiService::API_SUCCESS;
+                $status_message = "Menu Section updated successfully.";
+                $error_message = null;
+            } else {
+                $status_code = ApiService::API_NOT_FOUND;
+                $status_message = "Menu Section not found.";
+                $error_message = ["Menu Section not found."];
+            }
+
+        } catch (\Throwable $th) {
+            $status_code = ApiService::API_SERVER_ERROR;
+            $status_message = $th->getMessage();
+            $error_message = [$th->getMessage()];
+        }
+
+        return [$status_code, $status_message, $error_message];
+    }
+
+    public function deleteMenuSection($id)
+    {
+        $status_code = $status_message = null;
+
+        try {
+            [$status_code, $status_message] = self::getMenuSectionById($id);
+            MenuSection::where('id', $id)->delete();
+
+            $status_code = ApiService::API_SUCCESS;
+            $status_message = __('Menu Section deleted successfully.');
+        }
+        catch (\Throwable $th) {
+            $status_code = ApiService::API_SERVER_ERROR;
+            $status_message = $th->getMessage();
+        }
+
+        return [$status_code, $status_message];
+    }
+
 }
--- resources/views/backend/menu_section/form.blade.php
+++ resources/views/backend/menu_section/form.blade.php
@@ -6,9 +6,9 @@
     <div class="row">
         <div class="col-md-4 col-lg-4 col-12">
             <div class="mb-3">
-                <label for="sl" class="form-label">Sl</label>
+                <label for="sl" class="form-label">Sl</label><span class="text-danger">*</span>
                 <input type="number" class="form-control @error('sl') is-invalid @enderror" id="sl" name="sl"
-                    placeholder="Enter Sl" value="{{ old('sl') }}">
+                    placeholder="Enter Sl" value="{{ old('sl', @$data->sl) }}">
                 @error('sl')
                     <div class="invalid-feedback">{{ $message }}</div>
                 @enderror
@@ -16,9 +16,9 @@
         </div>
         <div class="col-md-4 col-lg-4 col-12">
             <div class="mb-3">
-                <label for="name" class="form-label">Section Name</label>
+                <label for="name" class="form-label">Section Name</label><span class="text-danger">*</span>
                 <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name"
-                    placeholder="Enter Section Name" value="{{ old('name') }}">
+                    placeholder="Enter Section Name" value="{{ old('name', @$data->name) }}">
                     @error('name')
                     <div class="invalid-feedback">{{ $message }}</div>
                     @enderror
@@ -26,10 +26,10 @@
         </div>
         <div class="col-md-4 col-lg-4 col-12">
             <div class="mb-3">
-                <label for="status" class="form-label">Status</label>
+                <label for="status" class="form-label">Status</label><span class="text-danger">*</span>
                 <select class="form-select @error('status') is-invalid @enderror" id="status" name="status">
                     @foreach(\App\Models\MenuSection::STATUS as $key => $value)
-                        <option value="{{ $key }}" @if(old('status')==$key) selected @endif>{{ $value }}</option>
+                        <option value="{{ $key }}" {{ old('status', @$data->status) == $key ? 'selected' : '' }}>{{ $value }}</option>
                     @endforeach
                 </select>
                 @error('status')
